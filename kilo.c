e32b8cad5bbbfa7f9a98afa4474441eaf8c4ea01
mancuoj <mancuoj@gmail.com>
2026-07-15 23:32:01 +0800

    clean

--- kilo.c
+++ kilo.c
@@ -112,6 +112,8 @@ void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
     abAppend(ab, "~", 1);
+    abAppend(ab, "\x1b[K", 3);
+
     if (y < E.screenrows - 1) {
       abAppend(ab, "\r\n", 2);
     }
@@ -121,12 +123,13 @@ void editorDrawRows(struct abuf *ab) {
 void editorRefreshScreen(void) {
   struct abuf ab = ABUF_INIT;
 
-  abAppend(&ab, "\x1b[2J", 4);
+  abAppend(&ab, "\x1b[?25l", 6);
   abAppend(&ab, "\x1b[H", 3);
 
   editorDrawRows(&ab);
 
   abAppend(&ab, "\x1b[H", 3);
+  abAppend(&ab, "\x1b[?25h", 6);
 
   write(STDOUT_FILENO, ab.b, ab.len);
   abFree(&ab);
